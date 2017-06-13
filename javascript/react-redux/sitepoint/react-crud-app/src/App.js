1fb860d9066e84b6c2e842676583fdecfc56686b
Vinaya Bhattarai <vnbhattarai@protonmail.ch>
2017-06-13 18:49:06 +0300

    feat: Update App component index to render the components

--- javascript/react-redux/sitepoint/react-crud-app/src/App.js
+++ javascript/react-redux/sitepoint/react-crud-app/src/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import { NavLink, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import ContactListPage from './pages/contact-list-page';
+import ContactFormPage from './pages/contact-form-page';
 
-export default class App extends Component {
+class App extends Component {
   render() {
-    return <div><h1>Contact Manager</h1></div>;
+    return (
+      <Container>
+        <div className="ui two item menu">
+          <NavLink className="item" activeClassName="active" exact to="/">
+            Contacts List
+          </NavLink>
+          <NavLink className="item" activeClassName="active" exact to="/contacts/new">
+            Add Contact
+          </NavLink>
+        </div>
+        <Route exact path="/" component={ContactListPage} />
+        <Route path="/contacts/new" component={ContactFormPage} />
+        <Route path="/contacts/edit/:_id" component={ContactFormPage} />
+      </Container>
+    );
   }
 }
+
+export default App;
