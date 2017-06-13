2cd0b212d5d208c9d4fa0036b531274f1fd130ce
Vinaya Bhattarai <vnbhattarai@protonmail.ch>
2017-06-13 20:25:17 +0300

    feat: Updated redux store and index HOC

--- javascript/react-redux/sitepoint/react-crud-app/src/index.js
+++ javascript/react-redux/sitepoint/react-crud-app/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store';
 import 'semantic-ui-css/semantic.min.css';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
