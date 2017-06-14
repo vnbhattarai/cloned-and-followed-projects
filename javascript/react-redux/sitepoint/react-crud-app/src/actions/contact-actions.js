41aa5603b4bfed04a03fb5f254156afdbe1d9a32
Vinaya Bhattarai <vnbhattarai@protonmail.ch>
2017-06-14 20:06:27 +0300

    feat: Add contact delete action

--- javascript/react-redux/sitepoint/react-crud-app/src/actions/contact-actions.js
+++ javascript/react-redux/sitepoint/react-crud-app/src/actions/contact-actions.js
@@ -14,34 +14,43 @@ export function fetchContacts() {
 export function newContact() {
   return dispatch => {
     dispatch({
-      type: 'NEW_CONTACT'
-    })
-  }
+      type: 'NEW_CONTACT',
+    });
+  };
 }
 
 export function saveContact(contact) {
   return dispatch => {
     return dispatch({
       type: 'SAVE_CONTACT',
-      payload: client.post(url, contact)
-    })
-  }
+      payload: client.post(url, contact),
+    });
+  };
 }
 
 export function fetchContact(_id) {
   return dispatch => {
     return dispatch({
       type: 'FETCH_CONTACT',
-      payload: client.get(`${url}/${_id}`)
-    })
-  }
+      payload: client.get(`${url}/${_id}`),
+    });
+  };
 }
 
 export function updateContact(contact) {
   return dispatch => {
     return dispatch({
       type: 'UPDATE_CONTACT',
-      payload: client.put(`${url}/${contact._id}`, contact)
-    })
-  }
+      payload: client.put(`${url}/${contact._id}`, contact),
+    });
+  };
+}
+
+export function deleteContact(_id) {
+  return dispatch => {
+    return dispatch({
+      type: 'DELETE_CONTACT',
+      payload: client.delete(`${url}/${_id}`),
+    });
+  };
 }
