4ccceaabd6ee4369a55db679bc7afef292ad4e71
Vinaya Bhattarai <vnbhattarai@protonmail.ch>
2017-06-14 23:59:15 +0300

    refactor: Update PORT url

--- javascript/react-redux/sitepoint/react-crud-app/src/actions/index.js
+++ javascript/react-redux/sitepoint/react-crud-app/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const client = axios.create({
-  baseURL: 'http://localhost:3030',
+  baseURL: 'http://localhost:3035',
   headers: {
     'Content-Type': 'application/json',
   },
